b2c5df32666f434ddbdb55af42b91a95dca73670
Mugdha Gupta <mxg167030@utdallas.edu>
2018-11-06 12:38:02 -0600

    create custom view for to field

---
+++ UTDMer/app/src/main/java/com/utdisaster/utdmer/view/AutoCompleteContactView.java
@@ -0,0 +1,7 @@
+package com.utdisaster.utdmer.view;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatMultiAutoCompleteTextView;
+
+public class AutoCompleteContactView extends AppCompatMultiAutoCompleteTextView {
+}
