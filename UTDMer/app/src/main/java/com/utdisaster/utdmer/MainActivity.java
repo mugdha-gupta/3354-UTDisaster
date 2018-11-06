50c1b91b41cca6bddaf7cfda1a1fa7df9074f659
Cooper F. Le <cooper@cooperle.com>
2018-11-06 11:59:36 -0600

    Base UI flow

--- UTDMer/app/src/main/java/com/utdisaster/utdmer/MainActivity.java
+++ UTDMer/app/src/main/java/com/utdisaster/utdmer/MainActivity.java
@@ -1,15 +1,26 @@
 package com.utdisaster.utdmer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.RecyclerView;
+
+import com.utdisaster.utdmer.adapater.ConversationAdapter;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Open new message activity
+                Intent intent = new Intent(MainActivity.this, NewMessageActivity.class);
+                startActivity(intent);
             }
         });
     }
