5fa8831c2f0fe0bf33862a3d8d0788801efda49b
Cooper F. Le <cooper@cooperle.com>
2018-11-26 12:12:56 -0600

    remove unneeded view

--- UTDMer/app/src/main/java/com/utdisaster/utdmer/view/AutoCompleteContactView.java
+++
@@ -1,7 +0,0 @@
-package com.utdisaster.utdmer.view;
-
-import android.content.Context;
-import android.support.v7.widget.AppCompatMultiAutoCompleteTextView;
-
-public class AutoCompleteContactView extends AppCompatMultiAutoCompleteTextView {
-}
