8eb304333ec845ea0bb4ed8d37d0f5e6708eb992
Cooper F. Le <cooper@cooperle.com>
2018-11-30 23:27:30 -0600

    fix fab action
    fab action no longer crashes - now properly opens new_message activity

--- UTDMer/app/src/main/java/com/utdisaster/utdmer/NewMessageActivity.java
+++ UTDMer/app/src/main/java/com/utdisaster/utdmer/NewMessageActivity.java
@@ -2,7 +2,6 @@ package com.utdisaster.utdmer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class NewMessageActivity extends AppCompatActivity {
 
@@ -10,8 +9,6 @@ public class NewMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
