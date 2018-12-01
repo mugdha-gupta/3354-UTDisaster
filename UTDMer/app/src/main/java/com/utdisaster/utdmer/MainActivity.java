0d6eef82fc68867b23ced27c4bf1921fcccf6935
Cooper F. Le <cooper@cooperle.com>
2018-11-30 22:43:52 -0600

    fix master branch
    Messed some things up in my MR. This should return master to a working state.

--- UTDMer/app/src/main/java/com/utdisaster/utdmer/MainActivity.java
+++ UTDMer/app/src/main/java/com/utdisaster/utdmer/MainActivity.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
