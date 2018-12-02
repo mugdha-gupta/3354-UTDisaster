d733ebec283f0412ee5669b417393764f5f7bb1f
Cooper F. Le <cooper@cooperle.com>
2018-12-02 17:02:39 -0600

    implement BroadcastReceiver
    update message list anytime a message is received

--- UTDMer/app/src/main/java/com/utdisaster/utdmer/MainActivity.java
+++ UTDMer/app/src/main/java/com/utdisaster/utdmer/MainActivity.java
@@ -1,9 +1,13 @@
 package com.utdisaster.utdmer;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
@@ -24,12 +28,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayAdapter arrayAdapter;
-    ListView messageView;
+    private ArrayAdapter arrayAdapter;
+    private ListView messageView;
+    // Create broadcast receiver that updates message view anytime a sms is received
+    private BroadcastReceiver broadcaseReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateMessageView();
+        }
+    };
 
     // Enum to keep track of where the user is when they are requested sms permission
     enum RequestCode {
-        APPLICATION_LAUNCH, FAB_ACTION
+        APPLICATION_LAUNCH, FAB_ACTION, BROADCAST_RECEIVER
     }
 
     private boolean getSmsPermissions(RequestCode requestCode) {
@@ -37,12 +48,11 @@ public class MainActivity extends AppCompatActivity {
             // If permissions requested at application launch, we will need read permissions to display all sms
             case APPLICATION_LAUNCH:
                 // Check if permissions already granted
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED
-                    && ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED){
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED){
                     return true;
                 }  else {
                 // Requests permissions if not granted
-                    requestPermissions(new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, requestCode.ordinal());
+                    requestPermissions(new String[]{Manifest.permission.READ_SMS}, requestCode.ordinal());
                     return false;
                 }
             // If permissions requested by a FAB action, we will need send permissions to send a sms
@@ -53,6 +63,14 @@ public class MainActivity extends AppCompatActivity {
                     requestPermissions(new String[]{Manifest.permission.SEND_SMS}, requestCode.ordinal());
                     return false;
                 }
+            case BROADCAST_RECEIVER:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED) {
+                    return true;
+                }  else {
+                    requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, requestCode.ordinal());
+                    return false;
+                }
+
             default: return false;
         }
     }
@@ -63,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         // View to display snackbar message in
         View view = findViewById(android.R.id.content);
         // Check if permissions were granted
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             RequestCode code = RequestCode.values()[requestCode];
             // Check request context
             switch (code) {
@@ -73,17 +91,11 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     break;
                 case APPLICATION_LAUNCH:
-                    // Populate message list if recent action was application launch
-                    List<Sms> messageList = SmsUtility.getSmsInbox(getApplicationContext());
-                    if (messageList != null) {
-                        // Find message view
-                        messageView = findViewById(R.id._messageView);
-                        // Set array adapter for view
-                        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, messageList);
-                        messageView.setAdapter(arrayAdapter);
-                        // Force view to redraw
-                        messageView.invalidate();
-                    }
+                        updateMessageView();
+                    break;
+                case BROADCAST_RECEIVER:
+                    IntentFilter filter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+                    registerReceiver(broadcaseReceiver, filter);
                     break;
                 default:
                     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -102,12 +114,7 @@ public class MainActivity extends AppCompatActivity {
         // Request read permissions
         if(getSmsPermissions(RequestCode.APPLICATION_LAUNCH)) {
             // If granted, populate listview with messages
-            List<Sms> messageList = SmsUtility.getSmsInbox(getApplicationContext());
-            messageView = findViewById(R.id._messageView);
-            if(messageList!=null) {
-                arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, messageList);
-                messageView.setAdapter(arrayAdapter);
-            }
+            updateMessageView();
         }
         else {
             // If not granted, explain to the user why there is nothing to see
@@ -125,6 +132,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if(getSmsPermissions(RequestCode.BROADCAST_RECEIVER)) {
+            // Request receiver for received sms messages
+            IntentFilter filter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+            registerReceiver(broadcaseReceiver, filter);
+        }
+
 
         // Activate Fab
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -163,5 +177,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //////
+    //
+    // Reload message view with all sms messages
+    //
+    //////
+    private void updateMessageView() {
+        // Get list of messages
+        List<Sms> messageList = SmsUtility.getSmsInbox(getApplicationContext());
+        // Find message view
+        messageView = findViewById(R.id._messageView);
+        if(messageList!=null) {
+            // Create adapter to display message
+            arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, messageList);
+            // Replace message view with messages
+            messageView.setAdapter(arrayAdapter);
+        }
     }
 }
