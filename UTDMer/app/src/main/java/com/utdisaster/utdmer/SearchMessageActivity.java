43887fb434323a3d1c0b06ed1c45176b561cafe9
mugdha-gupta <mxg167030@utdallas.edu>
2018-12-06 00:21:57 -0600

    finished implementing search

--- UTDMer/app/src/main/java/com/utdisaster/utdmer/SearchMessageActivity.java
+++ UTDMer/app/src/main/java/com/utdisaster/utdmer/SearchMessageActivity.java
@@ -3,15 +3,24 @@ package com.utdisaster.utdmer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.utdisaster.utdmer.models.Sms;
+import com.utdisaster.utdmer.utility.SmsUtility;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchMessageActivity extends AppCompatActivity {
 
     private static EditText searchEditText;
     private static ListView searchResultListView;
     private static Button searchButton;
+    private static List<Sms> searchResults = new ArrayList<>();
+    private ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +31,47 @@ public class SearchMessageActivity extends AppCompatActivity {
         searchResultListView = (ListView) findViewById(R.id._search_result_list_view);
         searchButton = (Button) findViewById(R.id._search_button_enter);
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String searchString = searchEditText.getText().toString();
+                List<Sms> allMessages = SmsUtility.getAllMessages(getApplicationContext());
+
+                if(searchResults != null){
+                    searchResults = new ArrayList<>();
+                }
+                for(Sms message: allMessages){
+                    if(message.getMsg().contains(searchString))
+                        searchResults.add(message);
+                }
+
+                if(searchResults != null){
+                    // Create adapter to display message
+                    arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, searchResults);
+                    // Replace message view with messages
+                    searchResultListView.setAdapter(arrayAdapter);
+                }
             }
         });
+
+
+
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        searchResultListView = null;
+        arrayAdapter.clear();
 
-
-
+    }
 }
