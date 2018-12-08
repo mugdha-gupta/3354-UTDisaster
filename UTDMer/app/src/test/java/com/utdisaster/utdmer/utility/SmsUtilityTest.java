8113fd3e790e50b455c843fedb1f446208e384d0
Cooper F. Le <cooper@cooperle.com>
2018-12-07 20:06:00 -0600

    test sendMessage

--- UTDMer/app/src/test/java/com/utdisaster/utdmer/utility/SmsUtilityTest.java
+++ UTDMer/app/src/test/java/com/utdisaster/utdmer/utility/SmsUtilityTest.java
@@ -1,29 +1,70 @@
 package com.utdisaster.utdmer.utility;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.util.Log;
 
 import com.utdisaster.utdmer.models.Sms;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.lang.reflect.Field;
 import java.sql.Timestamp;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({SmsManager.class, SmsUtility.class, Log.class, Telephony.Sms.Sent.class})
 public class SmsUtilityTest {
     @Test
-    public void testSendMessage() {
+    public void testSendMessage() throws Exception {
+        Context context = mock(Context.class);
+        ContentResolver contentResolver = mock(ContentResolver.class);
+        SmsManager smsManager = mock(SmsManager.class);
+        SmsUtility.setContext(context);
+        when(context.getContentResolver()).thenReturn(contentResolver);
+        PowerMockito.mockStatic(SmsManager.class);
+        BDDMockito.given(SmsManager.getDefault()).willReturn(smsManager);
+        ContentValues values = PowerMockito.mock(ContentValues.class);
+        doNothing().when(values).put(any(String.class), any(String.class));
+        doNothing().when(values).put(any(String.class), any(Integer.class));
+        PowerMockito.whenNew(ContentValues.class).withNoArguments().thenReturn(values);
+        PowerMockito.mockStatic(Log.class);
+        Uri uri = mock(Uri.class);
+        Field field = PowerMockito.field(Telephony.Sms.Sent.class, "CONTENT_URI");
+        field.set(Telephony.Sms.Sent.class, uri);
         Sms sms = new Sms();
         sms.setAddress("4695550205");
         sms.setFolderName("sent");
         sms.setMsg("This is the msg");
         sms.setReadState(true);
         sms.setTime(new Timestamp(123456789));
-        //verify(SmsUtility.class, times(1)).addNewMessage(eq(sms));
-        assertNotNull(sms);
+
+
+        SmsUtility.sendMessage(sms);
+        // Verify sms is sent
+        verify(smsManager, times(1)).sendTextMessage("4695550205", null,
+                "This is the msg", null, null);
+        // Verify sent sms is saved into Sms content
+        verify(context, times(1)).getContentResolver();
+        verify(contentResolver, times(1)).insert(eq(Telephony.Sms.Sent.CONTENT_URI), any(ContentValues.class));
     }
 
     @Test
@@ -47,7 +88,7 @@ public class SmsUtilityTest {
         sms.setMsg("BODY");
         sms.setReadState(false);
         sms.setFolderName("sent");
-        sms.setTime(new Timestamp(Long.valueOf(0)));
+        sms.setTime(new Timestamp(0));
         assertEquals(sms, SmsUtility.parseSmsCursor(c));
     }
 }
