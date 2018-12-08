2ae8623f2bb5a16a037238e0fca12143cfbf2f51
mugdha-gupta <mxg167030@utdallas.edu>
2018-12-07 22:38:44 -0600

    added more tests

--- UTDMer/app/src/test/java/com/utdisaster/utdmer/models/SmsTest.java
+++ UTDMer/app/src/test/java/com/utdisaster/utdmer/models/SmsTest.java
@@ -5,7 +5,10 @@ import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(PowerMockRunner.class)
@@ -19,6 +22,95 @@ public class SmsTest {
         newSms.setTime(new Timestamp(500));
 
         assertTrue(oldSms.compareTo(newSms) < 0);
+
+    }
+
+    @Test
+    public void testEquals(){
+        Sms sms1 = new Sms();
+        sms1.setId("0");
+        sms1.setTime(new Timestamp(1));
+        sms1.setAddress("2");
+        sms1.setFolderName("3");
+        sms1.setMsg("4");
+        sms1.setReadState(false);
+
+        // Exactly same as Sms 1
+        Sms sms2 = new Sms();
+        sms2.setId("0");
+        sms2.setTime(new Timestamp(1));
+        sms2.setAddress("2");
+        sms2.setFolderName("3");
+        sms2.setMsg("4");
+        sms2.setReadState(false);
+
+        assertTrue(sms1.equals(sms2));
+
+        // Id is different
+        Sms sms3 = new Sms();
+        sms2.setId("1");
+        sms2.setTime(new Timestamp(1));
+        sms2.setAddress("2");
+        sms2.setFolderName("3");
+        sms2.setMsg("4");
+        sms2.setReadState(false);
+
+        assertFalse(sms1.equals(sms3));
+
+    }
+
+    @Test
+    public void testToString(){
+        Sms sms1 = new Sms();
+        String id, address, folder, msg;
+        boolean readState;
+        Timestamp time;
+
+        id = "0";
+        address = "1";
+        msg = "2";
+        time = new Timestamp(500);
+        folder = "inbox";
+
+        sms1.setId(id);
+        sms1.setAddress(address);
+        sms1.setMsg(msg);
+        sms1.setTime(time);
+        sms1.setFolderName(folder);
+
+        String expected = "From: " + address + "\tId: " + id + "\nMessage: " + msg + "\nTimestamp: " + time;
+        assertEquals(sms1.toString(), expected);
+
+        folder = "sent";
+        sms1.setFolderName(folder);
+
+        expected = "To: " + address + "\tId: " + id + "\nMessage: " + msg + "\nTimestamp: " + time;
+        assertEquals(sms1.toString(), expected);
+
+
+        readState = false;
+        folder = "other";
+        sms1.setReadState(false);
+        sms1.setFolderName("other");
+
+
+        expected = "Sms{" + "id='" + id + '\'' + ", address='" + address + '\'' + ", msg='" + msg + '\'' +", readState=" + readState +
+                ", time=" + time + ", folderName='" + folder + '\'' + '}';
+        assertEquals(sms1.toString(), expected);
+    }
+
+    @Test
+    public void testHashCode(){
+        Sms sms1 = new Sms();
+        sms1.setId("0");
+        sms1.setTime(new Timestamp(1));
+        sms1.setAddress("2");
+        sms1.setFolderName("3");
+        sms1.setMsg("4");
+        sms1.setReadState(false);
+
+        Timestamp time = new Timestamp(1);
+        assertEquals(Objects.hash("0", "2", "4", false, time, "3"), sms1.hashCode());
 
     }
 }
